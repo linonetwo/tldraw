db3cce779c01a255e48a5c9efef8a1c45e594e4f
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-03-29 21:11:01 +0100

    WIP

--- packages/editor/src/lib/editor/Editor.ts
+++ packages/editor/src/lib/editor/Editor.ts
@@ -6218,6 +6218,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 		options: TLResizeShapeOptions = {}
 	): this {
 		const id = typeof shape === 'string' ? shape : shape.id
+		const now = Date.now()
 		if (this.getInstanceState().isReadonly) return this
 
 		if (!Number.isFinite(scale.x)) scale = new Vec(1, scale.y)
@@ -6353,6 +6354,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 				{ squashing: true }
 			)
 		}
+		console.log('resigin shape', id, Date.now() - now)
 
 		return this
 	}
